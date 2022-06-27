bd2b5056d0d4135b6e038f2018c0568629230c4f
root <root@Lenovo-Pro13.localdomain>
2022-06-27 15:57:00 +0800

    my work on lab2_challenge1 is done.

--- kernel/process.h
+++ kernel/process.h
@@ -26,6 +26,7 @@ typedef struct process_t {
   pagetable_t pagetable;
   // trapframe storing the context of a (User mode) process.
   trapframe* trapframe;
+  int stack_map_page_num;
 }process;
 
 // switch to run user app
--- user/app_sum_sequence.c
+++ user/app_sum_sequence.c
@@ -30,7 +30,7 @@ int main(void) {
   // SECOND, we use array out of bound to trigger pagefaults in an invalid address
   int *ans = (int *)naive_malloc();
 
-  printu("Summation of an arithmetic sequence from 0 to %ld is: %ld \n", n, sum_sequence(n+1, ans) );
+  printu("Summation of an arithmetic sequence from 0 to %ld is: %ld \n", n+1, sum_sequence(n+1, ans) );
 
   exit(0);
 }
